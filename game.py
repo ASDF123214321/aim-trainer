95db13af12c1db071f61719dfe3e6dc452bc0597
ASDF123214321 <102846821+ASDF123214321@users.noreply.github.com>
2022-08-06 18:17:31 +0930

    Update game.py
    Added Text - Points on the screen

--- game.py
+++ game.py
@@ -1,8 +1,13 @@
 import pygame,random,os
 pygame.init()
+
 bg = (0,0,0)
+
 enemy_x = random.randint(0, 640 - 50)
 enemy_y = random.randint(0, 480 - 50)
+player_size = 50
+player_width = player_size
+player_height = player_size
 points = 0
 
 player_y = 50
@@ -11,8 +16,12 @@ pygame.display.set_caption("some random aim trainer")
 screen = pygame.display.set_mode((640,480))
 while True:
   screen.fill((0,0,0))
-
-  player = pygame.draw.rect(screen,(0,0,0),(player_x, player_y, 50,50))
+  font = pygame.font.Font('freesansbold.ttf', 32)
+  text = font.render(str(points), True, (255,255,255), (0,0,0))
+  textRect = text.get_rect()
+  textRect.center = (320, 240)
+  player = pygame.draw.rect(screen,(0,0,0),(player_x, player_y, player_width, player_height))
+  screen.blit(text, textRect)
   enemy = pygame.draw.rect(screen,(255, 0, 0),(enemy_x, enemy_y , 50,50))
 
   pygame.display.update()
@@ -21,25 +30,14 @@ while True:
         if key.type==pygame.QUIT:
             exit()
   keys = pygame.key.get_pressed()
+  if keys[pygame.K_ESCAPE]:
+    exit()
   mousemoved = pygame.mouse.get_focused()
   if mousemoved:
      mouse_pos = pygame.mouse.get_pos()
-     player_x = mouse_pos[0] - 25
-     player_y = mouse_pos[1] - 25
+     player_x = mouse_pos[0] - player_width / 2
+     player_y = mouse_pos[1] - player_height / 2
 
-  if key.type == pygame.KEYDOWN:
-            if key.key == pygame.K_LEFT or key.key == pygame.K_a:
-                player_x -= 1
-                
-            if key.key == pygame.K_RIGHT or key.key == pygame.K_d:
-                player_x += 1
-                
-            if key.key == pygame.K_UP or key.key == pygame.K_w:
-                player_y -= 1
-                
-            if key.key == pygame.K_DOWN or key.key == pygame.K_s:
-                player_y += 1
-                
   def alert():
     print("crash: wall")
 #   if player_y > 431:
@@ -56,9 +54,6 @@ while True:
 #        alert()
   if player.collidepoint(enemy.center):
       points+=1
-      # kill the enemy
       enemy_x = random.randint(0, 640 - 50)
       enemy_y = random.randint(0, 480 - 50)
       pygame.display.update()
-      os.system('cls')
-      print(f"Points: {points}")
